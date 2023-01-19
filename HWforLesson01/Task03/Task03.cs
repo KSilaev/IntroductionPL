b4b62bcde4848042be8f6fcccf883d0c52531dce
Silaev Kirill <s-kir@yandex.ru>
2023-01-19 10:05:25 +0300

    Third task

---
+++ HWforLesson01/Task03/Task03.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+Console.Write("Введите число : ");
+string Num = Console.ReadLine();
+int Number = Convert.ToInt32(Num);
+
+if (Number % 2 == 0)
+{
+  System.Console.WriteLine($"Число {Num} четное");
+}
+else
+{
+  System.Console.WriteLine($"Число {Num} нечетное");
+}
