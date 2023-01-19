dd44707ef5e79ea41224a295f2b0781b7fa99a50
Silaev Kirill <s-kir@yandex.ru>
2023-01-19 10:27:26 +0300

    HW_01 Task_04

---
+++ HWforLesson01/Task04/Task04.cs
@@ -0,0 +1,11 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N
+
+Console.Write("Введите число : ");
+string Num = Console.ReadLine();
+int N = Convert.ToInt32(Num);
+
+System.Console.Write($"Четные числа до {N} включительно: ");
+for (int i = 2; i <= N; i+=2)
+{
+  System.Console.Write($"{i}; ");
+}
