2c9ac6b280b50dbba936206a9c50cba087279c5b
Silaev Kirill <s-kir@yandex.ru>
2023-01-24 17:57:40 +0300

    HW_03 Task_01

---
+++ HWforLesson03/HW3_Task01/HW3_Task01.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Не используйте строки
+
+int InputIntNumber(string Mes)
+{
+  System.Console.Write(Mes + " : ");
+  int IntNumber = Convert.ToInt32((System.Console.ReadLine()));
+  if (IntNumber > 0)
+  {
+    return IntNumber;
+  }
+  else
+  {
+    System.Console.WriteLine("Вы ввели отрицательное число!");
+    return -1;
+  }
+}
+
+int ReverseNumber(int Number)
+{
+  int i, Digit, ReverseNum = 0;
+  for (i = Number; i > 0; i = i / 10)
+  {
+    Digit = i % 10;
+    ReverseNum = (ReverseNum * 10) + Digit;
+  }
+  return ReverseNum;
+}
+
+int intNumber = InputIntNumber("Введите целое положительное число");
+
+if (intNumber != -1)
+{
+  if (intNumber == ReverseNumber(intNumber))
+  {
+    Console.WriteLine($"Введенное число {intNumber} является палиндромом");
+  }
+  else
+  {
+    Console.WriteLine($"Введенное число {intNumber} не является палиндромом!");
+  }
+}
