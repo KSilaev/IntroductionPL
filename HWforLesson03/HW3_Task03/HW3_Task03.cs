acde468c37a17ec0793b29dead383587a1af7982
Silaev Kirill <s-kir@yandex.ru>
2023-01-24 19:57:54 +0300

    HW_03 Task_03

--- HWforLesson03/HW3_Task03/HW3_Task03.cs
+++ HWforLesson03/HW3_Task03/HW3_Task03.cs
@@ -1,2 +1,21 @@
 ﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 
+// Проверку на ввод 0 не стал делать
+
+System.Console.Write("Ввведите число : ");
+int Num = Convert.ToInt32((System.Console.ReadLine()));
+
+if (Num > 0)
+{
+  for (int i = 1; i <= Num; i++)
+  {
+    System.Console.Write(Math.Pow(i, 3) + ", ");
+  }
+}
+else
+{
+  for (int i = -1; i >= Num; i--)
+  {
+    System.Console.Write(Math.Pow(i, 3) + ", ");
+  }
+}
