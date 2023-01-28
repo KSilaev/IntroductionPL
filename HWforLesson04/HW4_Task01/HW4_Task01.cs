c07b6a4b59a267ff663b40c0f841ef77ff4e2ee9
Silaev Kirill <s-kir@yandex.ru>
2023-01-28 08:04:54 +0300

    HW_04 Task_01

---
+++ HWforLesson04/HW4_Task01/HW4_Task01.cs
@@ -0,0 +1,30 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Реализовать через функции
+
+int InputIntNumber(string Mes)
+{
+  System.Console.Write(Mes + " : ");
+  int IntNumber = Convert.ToInt32((System.Console.ReadLine()));
+  if (IntNumber > 0)
+  {
+    return IntNumber;
+  }
+  else
+  {
+    System.Console.WriteLine("Вы ввели отрицательное число!");
+    return -1;
+  }
+}
+
+int ToRank(int X, int Y)
+{
+  int Result = 1;
+  for (int i = 1; i <= Y; i++)
+  {
+    Result = Result * X;
+  }
+  return Result;
+}
+
+int IntNumber = InputIntNumber("Введите целое положительное число");
+int Rank = InputIntNumber("Введите целое положительное число - степень, в которую будем возводить");
+System.Console.WriteLine($"Результатом возведения числа {IntNumber} в степень {Rank} будет {ToRank(IntNumber,Rank)}");
