a9edbf5cac9d6b3db94b8c37da3c9d8defa850e1
Silaev Kirill <s-kir@yandex.ru>
2023-01-28 08:20:54 +0300

    HW_04 Task_02

--- HWforLesson04/HW4_Task02/HW4_Task02.cs
+++ HWforLesson04/HW4_Task02/HW4_Task02.cs
@@ -1,2 +1,36 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе. Реализовать через функции
 
+int InputIntNumber(string Mes)
+{
+  System.Console.Write(Mes + " : ");
+  int IntNumber = Convert.ToInt32((System.Console.ReadLine()));
+  if (IntNumber > 0)
+  {
+    return IntNumber;
+  }
+  else
+  {
+    System.Console.WriteLine("Вы ввели отрицательное число!");
+    return -1;
+  }
+}
+
+int DigitSum(int Number)
+{
+  int Result = 0;
+  int Digit;
+  while (Number > 0)
+  {
+    Digit = Number % 10;
+    Result = Result + Digit;
+    Number = Number / 10;
+  }
+  return Result;
+}
+
+int IntNumber = InputIntNumber("Введите целое положительное число");
+
+if (IntNumber != -1)
+{
+  System.Console.WriteLine($"Сумма цифр числа {IntNumber} равна {DigitSum(IntNumber)}");
+}
