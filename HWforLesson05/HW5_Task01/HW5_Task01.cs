2eac3249f57940b2cb6eb15ff26fb60110269e2f
Silaev Kirill <s-kir@yandex.ru>
2023-01-28 09:34:57 +0300

    HW_05 Task_01

---
+++ HWforLesson05/HW5_Task01/HW5_Task01.cs
@@ -0,0 +1,37 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] FillArr(int Count, int Min, int Max)
+{
+  int[] ArrNums = new int[Count];
+  Random rnd = new Random();
+  for (int i = 0; i < Count; i++)
+  {
+    ArrNums[i] = rnd.Next(Min, Max);
+  }
+  return ArrNums;
+}
+
+void PrintArr(int[] Arr)
+{
+  int Count = Arr.Length;
+  for (int i = 0; i < Count; i++)
+  {
+    System.Console.WriteLine($"Элемент массива {i} равен {Arr[i]}");
+  }
+  //  return;
+}
+
+int[] RndArr;
+int OddCount = 0;
+
+RndArr = FillArr(7, 100, 999);
+//Выведем массив на экран, чтобы проверить работу программы
+PrintArr(RndArr);
+for (int i = 0; i < RndArr.Length; i++)
+{
+  if (RndArr[i] % 2 == 0)
+  {
+    OddCount++;
+  }
+}
+System.Console.WriteLine($"Количество четных чисел в массиве {OddCount}");
