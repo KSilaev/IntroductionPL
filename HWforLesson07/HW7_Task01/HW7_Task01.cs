2ac76cc6c2ecdfa91bea8e85278baa7eda67f71e
Silaev Kirill <s-kir@yandex.ru>
2023-02-06 22:51:22 +0300

    HW_07 Task_01

---
+++ HWforLesson07/HW7_Task01/HW7_Task01.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+// Заполнеим массив случайными вещественными числами
+void Fill2DArray(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(-100, 100) / 10.0;
+    }
+  }
+}
+
+//  Функция вывода элементов массива на терминал
+void Print2DArray(double[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+  }
+  Console.WriteLine();
+}
+
+Console.Write("Введите количество строк в массиве: ");
+int numberOfRows = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов в массиве: ");
+int numberOfColumns = Convert.ToInt32(Console.ReadLine());
+
+double[,] Matrix = new double[numberOfRows, numberOfColumns];
+Fill2DArray(Matrix);
+Print2DArray(Matrix);
